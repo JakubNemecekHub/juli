53eb46db9ea0d6990f4a60e02ccd1cf44117f3f2
Jakub Nemecek <jakubnemecek@hotmail.com>
2021-12-08 17:08:24 +0100

    Join Puase an UnPause
    Pause and Unpause buttons are now joined into one Pause button.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import enum
 import tkinter as tk
 from tkinter.constants import SINGLE, VERTICAL
 
@@ -6,6 +7,13 @@ import pygame
 
 
 root = tk.Tk()
+
+
+class PlaybackStatus(enum.Enum):
+    STOPPED = "Stopped"
+    PLAYING = "Playing"
+    PAUSED = "Paused"
+
 
 class MusicPlayer():
 
@@ -15,6 +23,7 @@ class MusicPlayer():
         self.root.geometry("1000x200+100+100") # Height x Width + x + y positions
         pygame.init()
         self.track = tk.StringVar()
+        self.playback_status = PlaybackStatus.STOPPED
         self.status = tk.StringVar()
 
         # Track Frame for song label and status
@@ -28,8 +37,6 @@ class MusicPlayer():
         frame_button.place(x=0, y=100, width=600, height=100)
         btn_play = tk.Button(frame_button, text="Play", command=self.playsong, width=10, height=1).grid(row=0, column=0, padx=10, pady=5)
         btn_pause = tk.Button(frame_button, text="Pause", command=self.pausesong, width=10, height=1).grid(row=0, column=1, padx=10, pady=5)
-        # Unpause zatím takto vyřešit nějakým vnitřním stavem a zapracovat do pause tlačítka
-        btn_unpause = tk.Button(frame_button, text="UnPause", command=self.unpausesong, width=10, height=1).grid(row=0, column=2, padx=10, pady=5)
         btn_stop = tk.Button(frame_button, text="Stop", command=self.stopsong, width=10, height=1).grid(row=0, column=3, padx=10, pady=5)
 
         # Playlist Frame
@@ -42,30 +49,34 @@ class MusicPlayer():
         self.playlist.pack(fill=tk.BOTH)
 
         # Song Directory
-        os.chdir("/home/jakub/Music/Blackmores Night/Natures Light (2001)") # User needs to be able to select this folder
+        os.chdir("/home/jakub/Music/Blackmores Night/Natures Light (2001)")
         # Fetch Songs
-        song_tracks = os.listdir()  # List all types of files
+        song_tracks = os.listdir()
         # Populate Playlist
         for track in song_tracks:
             self.playlist.insert(tk.END, track)
 
+    def _set_status(self, status: enum.Enum):
+        self.status.set(status.value)
+        self.playback_status = status
+
     def playsong(self):
         self.track.set(self.playlist.get(tk.ACTIVE))    # Display selected song
-        self.status.set("Playing")                      # Display status
+        self._set_status(PlaybackStatus.PLAYING)
         pygame.mixer.music.load(self.playlist.get(tk.ACTIVE))  # Load selected song
         pygame.mixer.music.play()                              # Play the song
 
     def pausesong(self):
-        self.status.set("Paused")
-        pygame.mixer.music.pause()
-
-    def unpausesong(self):
-        self.status.set("Playing again")
-        pygame.mixer.music.unpause()
+        if self.playback_status == PlaybackStatus.PLAYING:
+            pygame.mixer.music.pause()
+            self._set_status(PlaybackStatus.PAUSED)
+        elif self.playback_status == PlaybackStatus.PAUSED:
+            pygame.mixer.music.unpause()
+            self._set_status(PlaybackStatus.PLAYING)
 
     def stopsong(self):
-        self.status.set("Stopped")
         pygame.mixer.music.stop()
+        self._set_status(PlaybackStatus.STOPPED)
 
 
 if __name__ == "__main__":
